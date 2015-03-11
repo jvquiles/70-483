a9cb03d0b8ac3d377b738b04d9c0f4dd02e667e0
Juan Vicente Quiles Puchalt <jvquiles@gmail.com>
2015-03-11 23:48:31 +0100

    Chapter1

---
+++ Chapter1/Listing1_2.cs
@@ -0,0 +1,27 @@
+﻿namespace Objective1
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Threading;
+
+    [TestClass]
+    public class Listing1_2
+    {
+        static void ThreadMethod()
+        {
+            for (int i = 0; i < 20; i++) 
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        [TestMethod]
+        public void Main() 
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+            t.IsBackground = true;
+        }
+    }
+}
---
+++ Chapter1/Listing1_3.cs
@@ -0,0 +1,27 @@
+﻿namespace Objective1
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Threading;
+
+    [TestClass]
+    public class Listing1_3
+    {
+        static void ThreadMethod(object o)
+        {
+            for (int i = 0; i < (int)o; i++) 
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(0);
+            }
+        }
+
+        [TestMethod]
+        public void Main() 
+        {
+            Thread t = new Thread(new ParameterizedThreadStart(ThreadMethod));
+            t.Start(5);
+            t.Join();
+        }
+    }
+}
