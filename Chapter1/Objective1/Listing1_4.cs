78fdb478f873f183ac0dd8c68a34756843dd6191
Juan Vicente Quiles Puchalt <jvquiles@gmail.com>
2015-03-12 23:51:12 +0100

    Chapter1

---
+++ Chapter1/Objective1/Listing1_4.cs
@@ -0,0 +1,43 @@
+﻿namespace Chapter1.Objective1
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Threading;
+
+    [TestClass]
+    public class Listing1_4
+    {
+        static void ThreadMethod(object o)
+        {
+            for (int i = 0; i < (int)o; i++) 
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(0);
+            }
+        }
+
+        [TestMethod]
+        public void Main() 
+        {
+            bool stopped = false;
+
+            Thread t = new Thread(new ThreadStart(() => 
+            {
+                while(!stopped)
+                {
+                    Console.WriteLine("Running...");
+                    Thread.Sleep(1000);
+                }
+            }));
+
+            t.Start();
+            Console.WriteLine("Press any key to exit.");
+            //Console.ReadKey();
+
+            Thread.Sleep(10000);
+
+            stopped = true;
+            t.Join();
+        }
+    }
+}
---
+++ Chapter1/Objective1/Listing1_5.cs
@@ -0,0 +1,37 @@
+﻿namespace Chapter1.Objective1
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Threading;
+
+    [TestClass]
+    public class Listing1_5
+    {
+        [ThreadStatic]
+        public static int _field;
+
+        [TestMethod]
+        public void Main() 
+        {
+            new Thread(() => 
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    _field++;
+                    Console.WriteLine("Thread A: {0}", _field);
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    _field++;
+                    Console.WriteLine("Thread B: {0}", _field);
+                }
+            }).Start();
+
+            Thread.Sleep(10000);
+        }
+    }
+}
