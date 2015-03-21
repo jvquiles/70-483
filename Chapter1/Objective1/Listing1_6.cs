1396f8da001b00413cc8396d9f4c9dc1a48b663e
Juan Vicente Quiles Puchalt <jvquiles@gmail.com>
2015-03-21 07:55:06 +0100

    Chapter1

---
+++ Chapter1/Objective1/Listing1_6.cs
@@ -0,0 +1,38 @@
+﻿namespace Chapter1.Objective1
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Threading;
+
+    [TestClass]
+    public class Listing1_6
+    {
+        public static ThreadLocal<int> _field =
+            new ThreadLocal<int>(() =>
+            {
+                return Thread.CurrentThread.ManagedThreadId;
+            });
+
+        [TestMethod]
+        public void Main() 
+        {
+            new Thread(() => 
+            {
+                for (int x = 0; x < 10; x++)
+                {                    
+                    Console.WriteLine("Thread A: {0}", x);
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    Console.WriteLine("Thread B: {0}", x);
+                }
+            }).Start();
+
+            Thread.Sleep(10000);
+        }
+    }
+}
