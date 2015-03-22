61ed226f47e1691ad2ab363f47662f4e489616ae
Juan Vicente Quiles Puchalt <jvquiles@gmail.com>
2015-03-22 23:05:41 +0100

    Chapter 1

---
+++ Chapter1/Objective1/Listing1_15.cs
@@ -0,0 +1,48 @@
+﻿namespace Chapter1.Objective1
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class Listing1_15
+    {
+        [TestMethod]
+        public void Main() 
+        {
+            Task<int>[] tasks = new Task<int>[3];
+            tasks[0] = Task.Run(() => 
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("1");
+                return 1;
+            });
+            tasks[1] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("2");
+                return 2;
+            });
+            tasks[2] = Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("3");
+                return 3;
+            });
+
+            while(tasks.Length > 0)
+            {
+                int i = Task.WaitAny(tasks);
+                Task<int> completedTask = tasks[i];
+
+                Console.WriteLine(completedTask.Result);
+
+                var temp = tasks.ToList();
+                temp.RemoveAt(i);
+                tasks = temp.ToArray();
+            }
+        }
+    }
+}
