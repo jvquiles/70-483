3054ad15ff6c2b31c80ccaafc4a8dda6b040d14b
Juan Vicente Quiles Puchalt <jvquiles@gmail.com>
2015-03-23 23:37:12 +0100

    Chapter 1

---
+++ Chapter1/Objective1/Listing1_18.cs
@@ -0,0 +1,27 @@
+﻿namespace Chapter1.Objective1
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class Listing1_18
+    {
+        [TestMethod]
+        public void Main() 
+        {
+            string result = DownloadContent().Result;
+            Console.WriteLine(result);
+        }
+
+        public static async Task<string> DownloadContent() 
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string result = await client.GetStringAsync("http://www.microsoft.com");
+                return result;
+            }
+        }
+    }
+}
