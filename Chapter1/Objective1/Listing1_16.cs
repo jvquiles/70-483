2496f15d9507314d4679f10a5aad2395c2b8ade7
Juan Vicente Quiles Puchalt <jvquiles@gmail.com>
2015-03-23 23:04:14 +0100

    Chapter 1

---
+++ Chapter1/Objective1/Listing1_16.cs
@@ -0,0 +1,27 @@
+﻿namespace Chapter1.Objective1
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class Listing1_16
+    {
+        [TestMethod]
+        public void Main() 
+        {
+            Parallel.For(0, 10, i => 
+            {
+                Thread.Sleep(1000);
+            });
+
+            var numbers = Enumerable.Range(0, 10);
+            Parallel.ForEach(numbers, i => 
+            {
+                Thread.Sleep(1000);
+            });
+        }
+    }
+}
---
+++ Chapter1/Objective1/Listing1_17.cs
@@ -0,0 +1,26 @@
+﻿namespace Chapter1.Objective1
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class Listing1_17
+    {
+        [TestMethod]
+        public void Main() 
+        {
+            ParallelLoopResult result = Parallel.For(0, 1000, (int i, ParallelLoopState loopState) => 
+            {
+                if (i == 500) 
+                {
+                    Console.WriteLine("Breaking loop");
+                }
+
+                return;
+            });
+        }
+    }
+}
