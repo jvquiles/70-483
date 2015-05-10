2ba865b20c33d49056a015ba32555ad9dbd6c1bd
Juan Vicente Quiles Puchalt <jvquiles@gmail.com>
2015-05-10 23:47:07 +0200

    Chapter 1

---
+++ Chapter1/Objective4/Listing_75.cs
@@ -0,0 +1,28 @@
+﻿namespace Chapter1.Objective4
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+
+    [TestClass]
+    public class Listing_75
+    {
+        [TestMethod]
+        public void Main() 
+        {
+            this.UserDelegate();
+        }
+
+        public delegate int Calculate(int x, int y);
+        public int Add(int x, int y) { return x + y; }
+        public int Multiply(int x, int y) { return x * y; }
+
+        public void UserDelegate()
+        {
+            Calculate calc = Add;
+            Console.WriteLine(calc(3, 4));
+
+            calc = Multiply;
+            Console.WriteLine(calc(3, 4));
+        }
+    }
+}
