7097b2ea783c531d20d0ff271b4b72edc05a4efa
Juan Vicente Quiles Puchalt <jvquiles@gmail.com>
2015-06-01 23:16:46 +0200

    Chapter 2

---
+++ Chapter2/Objective1/Listing_13.cs
@@ -0,0 +1,47 @@
+﻿namespace Chapter2.Objective1
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+
+    [TestClass]
+    public class Listing_13
+    {
+        struct Nullable<T> where T : struct
+        {
+            private bool hasValue;
+            private T value;
+
+            public Nullable(T value) 
+            {
+                this.hasValue = true;
+                this.value = value;
+            }
+
+            public bool HasValue { get { return this.HasValue; } }
+
+            public T Value 
+            {
+                get 
+                {
+                    if (!this.HasValue) 
+                    {
+                        throw new ArgumentException();
+                    }
+
+                    return this.Value;
+                }
+            }
+
+            public T GetValueOrDefault() 
+            {
+                return this.Value;
+            }
+        }
+
+        [TestMethod]
+        public void Main() 
+        {
+            
+        }
+    }
+}
