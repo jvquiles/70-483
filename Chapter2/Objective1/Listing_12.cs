f6ed6e020512233534cb9420a1bb6428e2fcb107
Juan Vicente Quiles Puchalt <jvquiles@gmail.com>
2015-06-01 22:14:26 +0200

    Chapter 2

---
+++ Chapter2/Objective1/Listing_12.cs
@@ -0,0 +1,25 @@
+﻿namespace Chapter2.Objective1
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Listing_12
+    {
+        private class ConstructorChaining 
+        {
+            private int _p;
+
+            public ConstructorChaining() : this(3) { }
+            public ConstructorChaining(int p) 
+            {
+                this._p = p;
+            }
+        }
+
+        [TestMethod]
+        public void Main() 
+        {
+
+        }
+    }
+}
