ceaf05f7570efb9c52bf9a6485adce61049f66a2
Juan Vicente Quiles Puchalt <jvquiles@gmail.com>
2015-06-09 00:02:22 +0200

    Chapter 2

---
+++ Chapter2/Objective2/Listing_19.cs
@@ -0,0 +1,16 @@
+﻿namespace Chapter2.Objective2
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Listing_19
+    {
+        [TestMethod]
+        public void Main() 
+        {
+            int i = 42;
+            object o = i;
+            int x = (int)o;
+        }
+    }
+}
---
+++ Chapter2/Objective2/Listing_21.cs
@@ -0,0 +1,18 @@
+﻿namespace Chapter2.Objective2
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Net.Http;
+
+    [TestClass]
+    public class Listing_21
+    {
+        [TestMethod]
+        public void Main() 
+        {
+            HttpClient client = new HttpClient();
+            object o = client;
+            IDisposable d = client;
+        }
+    }
+}
