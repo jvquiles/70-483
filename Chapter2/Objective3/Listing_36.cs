5cfc8bea1ff41776ae4ba51a646573a89bab62f2
Juan Vicente Quiles Puchalt <jvquiles@gmail.com>
2015-06-14 20:05:44 +0200

    Chapter 2

---
+++ Chapter2/Objective3/Listing_36.cs
@@ -0,0 +1,26 @@
+﻿namespace Chapter2.Objective3
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Listing_36
+    {
+        private int _field;
+
+        public void SetValue(int value) 
+        {
+            this._field = value;
+        }
+
+        public int GetValue() 
+        {
+            return this._field;
+        }
+
+        [TestMethod]
+        public void Main() 
+        {
+
+        }
+    }
+}
---
+++ Chapter2/Objective3/Listing_37.cs
@@ -0,0 +1,34 @@
+﻿namespace Chapter2.Objective3
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+
+    [TestClass]
+    public class Listing_37
+    {
+        private class Person 
+        {
+            private string _firstName;
+
+            public string FirstName 
+            {
+                get { return this._firstName; }
+                set 
+                {
+                    if (string.IsNullOrEmpty(value)) 
+                    {
+                        throw new ArgumentException();
+                    }
+
+                    this._firstName = value;
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Main() 
+        {
+
+        }
+    }
+}
