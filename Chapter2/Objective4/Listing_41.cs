991df097ac7187553f1b18055d99161cf4da33ca
Juan Vicente Quiles Puchalt <jvquiles@gmail.com>
2015-06-24 23:47:16 +0200

    Chapter 2

---
+++ Chapter2/Objective4/Listing_41.cs
@@ -0,0 +1,47 @@
+﻿namespace Chapter2.Objective4
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+
+    [TestClass]
+    public class Listing_41
+    {
+        interface IExample 
+        {
+            string GetResult();
+            int Value { get; set; }
+            event EventHandler ResultRetrieved;
+            int this[string index] { get; set; }
+        }
+
+        private class ExampleImplementation : IExample 
+        {
+            public string GetResult() 
+            {
+                return "result";
+            }
+
+            public int Value { get; set; }
+
+            public event EventHandler CalculationPeformed;
+
+            public event EventHandler ResultRetrieved;
+
+            public int this[string index] 
+            {
+                get 
+                {
+                    return 42;
+                }
+
+                set { }
+            }
+        }
+
+        [TestMethod]
+        public void Main() 
+        {
+
+        }
+    }
+}
---
+++ Chapter2/Objective4/Listing_42.cs
@@ -0,0 +1,25 @@
+﻿namespace Chapter2.Objective4
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+
+    [TestClass]
+    public class Listing_42
+    {
+        interface IReadOnlyInterface 
+        {
+            int Value { get; }
+        }
+
+        struct ReadAndWriteImplementation : IReadOnlyInterface 
+        {
+            public int Value { get; set; }
+        }
+
+        [TestMethod]
+        public void Main() 
+        {
+
+        }
+    }
+}
