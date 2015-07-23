37ceb6bb0baf62c2d321a923bf49c75f68d42e14
Juan Vicente Quiles Puchalt <jvquiles@gmail.com>
2015-07-23 23:29:16 +0200

    Chapter 2

---
+++ Chapter2/Objective5/Listing_77.cs
@@ -0,0 +1,16 @@
+﻿namespace Chapter2.Objective5
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+
+    [TestClass]
+    public class Listing_77
+    {
+        [TestMethod]
+        public void Main() 
+        {
+            Func<int, int, int> addFunc = (x, y) => x + y;
+            Console.WriteLine(addFunc(2, 3));
+        }
+    }
+}
---
+++ Chapter2/Objective5/Listing_78.cs
@@ -0,0 +1,27 @@
+﻿namespace Chapter2.Objective5
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Linq.Expressions;
+
+    [TestClass]
+    public class Listing_78
+    {
+        [TestMethod]
+        public void Main() 
+        {
+            BlockExpression blockExpr = Expression.Block(
+                Expression.Call(
+                    null,
+                    typeof(Console).GetMethod("Write", new Type[] { typeof(string) }),
+                    Expression.Constant("Hello ")),
+                Expression.Call(
+                    null,
+                    typeof(Console).GetMethod("WriteLine", new Type[] { typeof(string) }),
+                    Expression.Constant("World!"))
+                    );
+
+            Expression.Lambda<Action>(blockExpr).Compile();
+        }
+    }
+}
