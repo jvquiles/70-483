59b208ea3ddef18defe638b5c43acebdd1600794
Juan Vicente Quiles Puchalt <jvquiles@gmail.com>
2015-07-27 18:48:38 +0200

    Chapter 2

---
+++ Chapter2/Objective6/Listing_79.cs
@@ -0,0 +1,22 @@
+﻿namespace Chapter2.Objective6
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Listing_79
+    {
+        private class SomeType 
+        {
+            ~SomeType() 
+            {
+                // This code is called when the finalize method executes
+            }
+        }
+
+        [TestMethod]
+        public void Main ()
+        {
+
+        }
+    }
+}
